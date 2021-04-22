baa8a379e75765d5da747bdb435613dcf10ccfdd
teirm <tribunus.vii@gmail.com>
2021-04-21 21:35:31 -0400

    Process on accepting connections -- still haven't added to the build yet

--- src/server/Acceptor.hpp
+++ src/server/Acceptor.hpp
@@ -6,6 +6,9 @@
 
 
 #include <string>
+#include <thread>
+#include <future>
+#include <atomic>
 
 #include <sys/socket.h>
 
@@ -14,23 +17,25 @@ public:
     Acceptor(int socket_fd):
         socket_(socket_fd),
         epoll_fd_(0),
+        read_pipe_(0),
+        write_pipe_(0),
         is_running_(false) { }
     Acceptor(const Acceptor &rhs) = delete;
     Acceptor& operator=(const Acceptor &rhs) = delete;
     ~Acceptor();
     
-    int operator()();
-
-    // TODO: stop() should manage the pipe internally
-    //       to break out of the select loop. Error of the
-    //       acceptor loop should be handled differently...
-    //       perhaps a future?
-    //       Coordinating shutdown / errors is tricky.
+    int start();
     int stop();
 
 private:
     int socket_;
     int epoll_fd_;
-    bool is_running_;
+    int read_pipe_;
+    int write_pipe_;
+    std::atomic<bool> is_running_;
+    std::thread acceptor_;
+
     void accept_connections();
+    int handle_new_connection();
+
 };
