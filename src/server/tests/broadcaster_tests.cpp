aef5b2a7fd11599c6e847a7afd83c89144eb0c98
teirm <tribunus.vii@gmail.com>
2021-06-27 19:02:42 -0400

    test for direct messages

--- src/server/tests/broadcaster_tests.cpp
+++ src/server/tests/broadcaster_tests.cpp
@@ -22,6 +22,9 @@
 
 const static int TEST_FD = 10;
 const static unsigned int NUM_CLIENTS = 4;
+    
+using client_container_t = std::vector<std::pair<std::string, Channel>>;
+using event_container_t  = std::vector<io_mplex_fd_info_t>;
 
 TEST_CASE("broadcaster is createable", "[construct]") {
     REQUIRE_NOTHROW(BroadCaster());
@@ -39,10 +42,6 @@ TEST_CASE("broadcaster add/delete client", "[add-del-client]") {
 }
 
 TEST_CASE("broadcaster broadcast message", "[broadcast-msg]") {
-    
-    using client_container_t = std::vector<std::pair<std::string, Channel>>;
-    using event_container_t  = std::vector<io_mplex_fd_info_t>;
-    
     auto broad_caster = BroadCaster();
     auto io_mplex = IoMultiplexorFactory::get_multiplexor(NUM_CLIENTS);
     client_container_t test_clients(NUM_CLIENTS);
@@ -56,14 +55,14 @@ TEST_CASE("broadcaster broadcast message", "[broadcast-msg]") {
         REQUIRE(io_mplex->add({0, MPLEX_IN | MPLEX_ONESHOT, test_client.second.read_pipe}) == 0);
         INFO("Client: " << test_client.first << " fd: " << test_client.second.read_pipe);
     }
-    INFO(test_clients.size());
     
     // broadcast a message from a single client
     auto &sending_client = test_clients.at(2);
     REQUIRE(io_mplex->remove(sending_client.second.read_pipe) == 0);
-    
+
     message_t message{{4, 2, nullptr}, "moo"};
     broad_caster.broadcast_msg(sending_client.second.write_pipe, std::move(message));
+
     event_container_t  test_events(NUM_CLIENTS);
     int remaining_responses = NUM_CLIENTS - 1;
     while (remaining_responses > 0) {
@@ -84,4 +83,32 @@ TEST_CASE("broadcaster broadcast message", "[broadcast-msg]") {
         remaining_responses -= events;
         INFO("remaining events: " << remaining_responses);
     }
+}
+
+TEST_CASE("broadcaster direct message", "[direct-msg]") {
+    Channel sender;
+    Channel receiver;
+    auto broad_caster = BroadCaster();
+    
+    broad_caster.add_client("sender", sender.write_pipe);
+    broad_caster.add_client("receiver", receiver.write_pipe);   
+
+    auto io_mplex = IoMultiplexorFactory::get_multiplexor(2);
+    REQUIRE(io_mplex);
+    REQUIRE(io_mplex->add({0, MPLEX_IN | MPLEX_ONESHOT, receiver.read_pipe}) == 0);
+
+    broad_caster.direct_msg(sender.write_pipe, {{5, 2, "receiver"}, "test"});
+    
+    event_container_t test_events;
+    int events = io_mplex->wait(nullptr, test_events);
+    REQUIRE(events == 1);
+    REQUIRE(test_events.size() == 1);
+    
+    auto event = test_events.front();
+    REQUIRE(event.filters & MPLEX_IN);
+    
+    message_t received_msg;
+    REQUIRE(read_message(event.fd, received_msg) == 0);
+    REQUIRE(strncmp(received_msg.message, "test", strlen("moo")) == 0);
+    REQUIRE(close(event.fd) == 0);
 }
